5e9a2add5422a83fba3420f94f5b672c337bec07
ArshilHapani <arshilhapani998@gmail.com>
2024-10-14 23:36:03 +0530

    minimal account abs

--- account_abstraction__foundry/src/ethereum/MinimumAccount.sol
+++ account_abstraction__foundry/src/ethereum/MinimumAccount.sol
@@ -1,4 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-contract MinimumAccount {}
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+
+import {Errors} from "./utils/Errors.sol";
+
+contract MinimumAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        override
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce(userOp.nonce);
+        _payPrefund(missingAccountFunds);
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        // custom logic for signature validation (e.g. whitelist, session key,etc.)
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = msg.sender.call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
+}
