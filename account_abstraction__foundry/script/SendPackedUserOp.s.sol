19ef14645e2482df3b2299fb5ecb111cb2ab0efd
ArshilHapani <arshilhapani998@gmail.com>
2024-10-21 00:07:55 +0530

    script prepared

--- account_abstraction__foundry/script/SendPackedUserOp.s.sol
+++ account_abstraction__foundry/script/SendPackedUserOp.s.sol
@@ -1,2 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract SendPackedUserOp is Script {
+    function run() external {}
+
+    function generateUserOperation(bytes memory callData, address sender) public returns (PackedUserOperation memory) {
+        /**
+         * 1. Generate unsigned user operation
+         * 2. Sign it, and return it
+         */
+        uint256 nonce = vm.getNonce(sender);
+
+        PackedUserOperation memory unsignedUserOps = _generateUnsignedUserOperation(callData, sender, nonce);
+    }
+
+    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
+        internal
+        pure
+        returns (PackedUserOperation memory)
+    {
+        uint128 verificationGasLimit = 16777216;
+        uint128 callGasLimit = verificationGasLimit;
+        uint256 maxPriorityFeePerGas = 256;
+        uint256 maxFeePerGas = maxPriorityFeePerGas;
+
+        return PackedUserOperation({
+            sender: sender,
+            nonce: nonce,
+            initCode: hex"",
+            callData: callData,
+            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
+            preVerificationGas: verificationGasLimit,
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+            paymasterAndData: hex"",
+            signature: hex""
+        });
+    }
+}
--- account_abstraction__foundry/test/ethereum/MinimalAccountTest.t.sol
+++ account_abstraction__foundry/test/ethereum/MinimalAccountTest.t.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.25;
 
@@ -52,6 +52,7 @@ contract MinimalAccountTest is Test {
         vm.prank(randomUser);
         vm.expectRevert(Errors.MinimumAccount__RequireEntryPointToCallThisFunction.selector);
         minimalAccount.execute(dest, value, functionData);
+
         // assert
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
     }
